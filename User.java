017cc09b9f4e1b105c46ed162acb594f46f0c6c2
MrAbshar01 <mrabshar01@gmail.com>
2026-06-16 03:17:03 +0800

    Update User.java

--- User.java
+++ User.java
@@ -6,18 +6,6 @@ abstract class User {
     public abstract String getUserDetails();
 }
 
-class Student extends User {
-    private String matricNo;
-    public Student(String name, String matricNo) {
-        super(name);
-        this.matricNo = matricNo;
-    }
-    @Override
-    public String getUniqueId() { return matricNo; }
-    @Override
-    public String getUserDetails() { return "Student: " + name + " (Matric: " + matricNo + ")"; }
-}
-
 class Staff extends User {
     private String staffId;
     public Staff(String name, String staffId) {
