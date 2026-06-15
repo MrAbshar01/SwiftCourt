948ddae5be4cc6d260335791bd45895e9e3147dc
MrAbshar01 <mrabshar01@gmail.com>
2026-06-16 03:20:07 +0800

    Create Facility.java

---
+++ Facility.java
@@ -0,0 +1,7 @@
+abstract class Facility {
+    protected String facilityId;
+    protected String name;
+    protected double baseRate;
+    protected String status; 
+    protected int courtLimit;
+}
