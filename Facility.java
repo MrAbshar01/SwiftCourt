82ac8ab0941fd8e91a1dfd5f4c95bc61bcaba12e
MrAbshar01 <mrabshar01@gmail.com>
2026-06-16 03:21:50 +0800

    Add constructor and methods to Facility class

--- Facility.java
+++ Facility.java
@@ -4,4 +4,16 @@ abstract class Facility {
     protected double baseRate;
     protected String status; 
     protected int courtLimit;
+
+    public Facility(String facilityId, String name, double baseRate) {
+        this.facilityId = facilityId;
+        this.name = name;
+        this.baseRate = baseRate;
+        this.status = "Available";
+        this.courtLimit = 1;
+    }
+    public String getName() { return name; }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+    public abstract double calculateBookingFee(int hours);
 }
