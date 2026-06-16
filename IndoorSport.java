945dd073f0cc0e40d2927c3ccbd033d0847267ea
MrAbshar01 <mrabshar01@gmail.com>
2026-06-16 10:30:50 +0800

    Update IndoorSport.java

--- IndoorSport.java
+++ IndoorSport.java
@@ -1,11 +1,10 @@
 public class IndoorSport extends Facility {
     private boolean hasAC;
-    public IndoorSport(String facilityId, String name, double baseRate, boolean hasAC) {
+    public IndoorSport(String facilityId, String name, double baseRate) {
         super(facilityId, name, baseRate);
-        this.hasAC = hasAC;
     }
     @Override
     public double calculateBookingFee(int hours) {
-        return (baseRate * hours) + (hasAC ? (15.0 * hours) : 0);
+        return (baseRate * hours);
     }
 }
